4acfd163b3a9088cb56a1b0b52b876e17a976cda
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-13 20:10:10 -0600

    Add three fortune statements and include randomize function.

--- FortuneCookie/FortuneCookie.vb
+++ FortuneCookie/FortuneCookie.vb
@@ -3,13 +3,24 @@
 'Fall 2021
 'Fortune Cookie
 'https://github.com/heinelli/FortuneCookie.git
+
+Option Strict On
+Option Explicit On
 Module FortuneCookie
 
     Sub Main()
 
-        Dim rndNumber As Integer
-            rndNumber = Int((3 * Rnd()) + 1)    'Formula used to determine parameters:  Int((upperbound - lowerbound + 1) * Rnd + lowerbound)
-            Console.WriteLine(rndNumber)
+        Dim rndNumber As Single
+        Randomize()
+        rndNumber = Int((3 * Rnd()) + 1)    'Formula used to determine parameters:  Int((upperbound - lowerbound + 1) * Rnd + lowerbound)
+
+            If rndNumber = 1 Then
+                Console.WriteLine("Allow compassion to guide your decisions.")
+            ElseIf rndNumber = 2 Then
+                Console.WriteLine("Never let a problem to be solved become more important than a person to be loved.")
+            ElseIf rndNumber = 3 Then
+                Console.WriteLine("Get some exercise!")
+            End If
             Console.Read()
 
     End Sub
