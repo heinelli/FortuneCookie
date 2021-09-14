7523aa577b5f79c10952eabf9ad5677f9a29cac3
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-13 19:55:57 -0600

    Add project files.

---
+++ FortuneCookie/FortuneCookie.vb
@@ -0,0 +1,13 @@
+﻿
+Module FortuneCookie
+
+    Sub Main()
+
+        Dim rndNumber As Integer
+            rndNumber = Int((3 * Rnd()) + 1)    'Formula used to determine parameters:  Int((upperbound - lowerbound + 1) * Rnd + lowerbound)
+            Console.WriteLine(rndNumber)
+            Console.Read()
+
+    End Sub
+
+End Module
