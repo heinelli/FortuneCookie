7f9992dcbded9fc73ffc4afe0a6677689c65bdbb
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-18 22:25:16 -0600

    Add comments and tidy up code.

--- FortuneCookie/FortuneCookie.vb
+++ FortuneCookie/FortuneCookie.vb
@@ -11,18 +11,26 @@ Module FortuneCookie
     Sub Main()
 
         Dim rndNumber As Single
+
         Randomize()
-        rndNumber = Int((3 * Rnd()) + 1)    'Formula used to determine parameters:  Int((upperbound - lowerbound + 1) * Rnd + lowerbound)
+        'Random number algorithm is based on computer clock.
 
-            If rndNumber = 1 Then
-                Console.WriteLine("Allow compassion to guide your decisions.")
-            ElseIf rndNumber = 2 Then
-                Console.WriteLine("Never let a problem to be solved become more important than a person to be loved.")
-            ElseIf rndNumber = 3 Then
-                Console.WriteLine("Get some exercise!")
-            End If
-            Console.Read()
+        rndNumber = Int((8 * Rnd()) + 1)
+        'Formula used to determine parameters:  Int((upperbound - lowerbound + 1) * Rnd + lowerbound)
 
+        If rndNumber = 2 Or rndNumber = 7 Then
+            Console.WriteLine("Allow compassion to guide your decisions.")
+        ElseIf rndNumber = 1 Or rndNumber = 5 Then
+            Console.WriteLine("Never let a problem to be solved become more 
+important than a person to be loved.")
+        ElseIf rndNumber = 4 Or rndNumber = 6 Then
+            Console.WriteLine("Get some exercise!")
+        ElseIf rndNumber = 3 Or rndNumber = 8 Then
+            Console.WriteLine("The past is to be learned from but not lived in")
+        End If
+        'One of four quotes is displayed based on random number.
+
+        Console.Read()
     End Sub
 
 End Module
